74536d3c054d0cd28083b810ab8276243d54d5f4
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-11-08 12:24:06 -0800

    Update PacketField.php

--- PacketField.php
+++ PacketField.php
@@ -13,73 +13,80 @@
  * @author PEMapModder
  */
 
-class PacketField{
-//	private $message;
-//	private $instr, $modifier, $args;
-//	private $valid = false;
-	private $type;
-	private $size;
+class PacketField
+{
 
-	public function __construct($type, $size){
-		$this->type = $type;
-		$this->size = $size;
-	}
+    /*private $message;
+    private $instr, $modifier, $args;
+    private $valid = false;*/
+    private $type;
+    private $size;
 
-//	private $destination, $value;
+    public function __construct($type, $size)
+    {
+        $this->type = $type;
+        $this->size = $size;
+    }
 
-//	public function __construct($message, &$pkSize){
-//		$this->message = $message = trim($message);
-//		if(preg_match('@^[0-9a-f]+:[ \t]+(([0-9a-f]{4}[ \t]+){1,2})(([a-z]+)(\.([a-z]))?)[ \t]+(.+)$@', $message, $match)){
-//			$instr = $match[4];
-//			$modifier = $match[6];
-//			$args = $match[7];
-//			$this->instr = $instr;
-//			$this->modifier = $modifier;
-//			$this->args = $args;
-//			if($instr === "bl"){
-//				$types = [
-//					["PacketUtil::readString", "string"],
-//					["PacketUtil::readUUID", "uuid"],
-//					["PacketUtil::readItemInstance", "item"],
-//				];
-//				foreach($types as list($needle, $type)){
-//					if(strpos($message, $needle) !== false){
-//						$this->type = $type;
-//						$this->valid = true;
-//						return;
-//					}
-//				}
-//				if(strpos($message, "RakNet::BitStream::ReadBits") !== false){
-//					$this->type = "numeric";
-//					$this->size = $pkSize;
-//					$this->valid = true;
-//				}
-//			}
-//			if($instr === "movs"){
-//				list($dest, $val) = explode(", ", $args);
-//				$this->destination=$dest;$this->value=$val;$this->valid=true;
-//				if($dest === "r2"){
-//					$pkSize = intval(substr($val, 1));
-//				}
-//			}
-//		}
-//	}
+    //private $destination, $value;
 
-//	/**
-//	 * @return boolean
-//	 */
-//	public function isValid(){
-//		return $this->valid;
-//	}
+    /*public function __construct($message, &$pkSize)
+    {
+        $this->message = $message = trim($message);
+        if (preg_match('@^[0-9a-f]+:[ \t]+(([0-9a-f]{4}[ \t]+){1,2})(([a-z]+)(\.([a-z]))?)[ \t]+(.+)$@', $message, $match)) {
+            $instr = $match[4];
+            $modifier = $match[6];
+            $args = $match[7];
+            $this->instr = $instr;
+            $this->modifier = $modifier;
+            $this->args = $args;
+            if ($instr === "bl") {
+                $types = [
+                    ["PacketUtil::readString", "string"],
+                    ["PacketUtil::readUUID", "uuid"],
+                    ["PacketUtil::readItemInstance", "item"],
+                ];
+                foreach ($types as list($needle, $type)) {
+                    if (strpos($message, $needle) !== false) {
+                        $this->type = $type;
+                        $this->valid = true;
+                        return;
+                    }
+                }
+                if (strpos($message, "RakNet::BitStream::ReadBits") !== false) {
+                    $this->type = "numeric";
+                    $this->size = $pkSize;
+                    $this->valid = true;
+                }
+            }
+            if ($instr === "movs") {
+                list($dest, $val) = explode(", ", $args);
+                $this->destination=$dest;$this->value=$val;$this->valid=true;
+                if ($dest === "r2") {
+                    $pkSize = intval(substr($val, 1));
+                }
+            }
+        }
+    }*/
 
-	public function dumpInfo(){
-		return [
-//			"instruction" => $this->instr,
-//			"modifier" => $this->modifier,
-//			"args" => $this->args,
-			"type" => $this->type,
-			"size" => $this->size,
-//			"destination" => $this->destination, "value" => $this->value,
-		];
-	}
+    /**
+     * @return boolean
+     */
+    /*public function isValid()
+    {
+        return $this->valid;
+    }*/
+
+    public function dumpInfo()
+    {
+        return [
+            "instruction" => $this->instr,
+            //"modifier" => $this->modifier,
+            //"args" => $this->args,
+            "type" => $this->type,
+            "size" => $this->size,
+            //"destination" => $this->destination, "value" => $this->value,
+        ];
+    }
+
 }
